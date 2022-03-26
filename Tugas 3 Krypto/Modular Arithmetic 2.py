c1b654ec2c46072a0c4585a43a0a4af085ff85ed
salman <salmansudirlan@gmail.com>
2022-03-26 13:35:04 +0700

    kriptografi

--- Tugas 3 Krypto/Modular Arithmetic 2.py
+++ Tugas 3 Krypto/Modular Arithmetic 2.py
@@ -1,9 +1,1 @@
-def ex_gcd(a,b):
-    if a == 0:
-        return (b, 0, 1)
-    else:
-        (gcd, u, v) = ex_gcd(b % a, a)
-        return (gcd, v - (b// a) * u, u)
-a = 26513
-b = 32321
-print(ex_gcd(a,b))
+print(pow(273246787654,65536,65537))
